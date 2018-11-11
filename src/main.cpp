672e8a2423fcb99dc9391493c99373ead6600052
Luís Cláudio <luisclaudiogouveiarocha@gmail.com>
2018-11-11 13:11:38 -0300

    something is slightly wrong with the units, but maybe wrong results are due to narrow FoV

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,14 @@ int main(int argc, char** args)
   srand(0);
 
   // "load" scene -----------
+  // units are given in meters!!!
+  // QUESTION: with 35mm film and 35mm lens an object
+  // of radius r should be at a distance of 2r to be
+  // completely in scene, but this is not the case for
+  // these spheres, which must be at roughly 2.25r for
+  // this. why is this? Must test with planes after!!!
   Scene scene;
-  scene.prims.push_back( Shape(Vec3(0.0f,0.0f,-4.0f), 1.0f) );
+  scene.prims.push_back( Shape(Vec3(0.0f,0.0f,-0.9f), 0.4f) );
 
   // configure integrator and film settings ----------
   Integrator integrator;
